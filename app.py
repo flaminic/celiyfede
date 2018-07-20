ff841f775a0b02f3f10c4a3a74063da34328b351
Flaminia <flaminic@thoughtworks.com>
2018-07-20 15:35:06 -0300

    add database connection to add and modify rsvp

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from database import Database
 
 app = Flask(__name__)
+database = Database()
 
 @app.route("/")
 def home():
@@ -18,10 +20,52 @@ def bodas():
 def rsvp():
     return render_template('rsvp.html')\
 
+@app.route("/rsvp/<person_id>")
+def edit_rsvp(person_id):
+    person_details_info = database.get_person_details(person_id)
+    if person_details_info:
+        return render_template('editRsvp.html', person_details_info=person_details_info)
+    else:
+        return redirect(url_for('home'))
+
+@app.route("/thanks/<person_id>")
+def thanks(person_id):
+    return render_template('thanks.html', person_id=person_id)
+
 @app.route("/register", methods=['POST'])
 def register():
     if request.method == 'POST':
-        return render_template("thanks.html")
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        email = request.form['email']
+        howmany = request.form['people']
+        message = request.form['message'] or ""
+        person_id = database.insert_person(
+            first_name=first_name,
+            last_name=last_name,
+            email = email,
+            howmany = howmany,
+            message = message,
+        )
+        return redirect(url_for('thanks', person_id=person_id))\
+
+@app.route("/register/<person_id>", methods=['POST'])
+def modify_person(person_id):
+    if request.method == 'POST':
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        email = request.form['email']
+        howmany = request.form['people']
+        message = request.form['message'] or ""
+        database.edit_person(
+            person_id,
+            first_name=first_name,
+            last_name=last_name,
+            email = email,
+            howmany = howmany,
+            message = message,
+        )
+    return redirect(url_for('thanks', person_id=person_id))
 
 
 if __name__ == "__main__":
